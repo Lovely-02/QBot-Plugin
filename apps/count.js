618e6df3d4018738a8c7e57bdb7461df20c50ce5
02 <hentai02y@gmail.com>
2025-05-07 17:10:43 +0800

    🐞 fix: Bot统计头像

--- apps/count.js
+++ apps/count.js
@@ -26,7 +26,7 @@ export class Qcount extends plugin {
     if (!getUser) {
       await DB.setID("user", user)
       const userCount = await this.getall("user")
-      const data = await Bot.pickMember(group, String(e.user_id)).getAvatarUrl(100)
+      const data = await Bot.pickMember(String(e.group_id), String(e.user_id)).getAvatarUrl(100)
       const url = await data.replace(/\/0$/, "/100")
       const msg = [
         segment.image(url),
